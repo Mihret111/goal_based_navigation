915c4ec7b09deaccd67bfaf4f54e324ac4bc1b29
Mihret111 <mihret.kochito16@gmail.com>
2026-04-24 11:24:05 +0200

    complete base skeleton for action server

--- nav_system/include/nav_system/nav_action_server_component.hpp
+++ nav_system/include/nav_system/nav_action_server_component.hpp
@@ -1,4 +1,4 @@
-#ifndef nav_system__NAV_ACTION_SERVER_COMPONENT_HPP_
+#ifndef nav_system__NAV_ACTION_SERVER_COMPONENT_HPP_   
 #define nav_system__NAV_ACTION_SERVER_COMPONENT_HPP_
 
 #include <memory>
@@ -12,27 +12,31 @@
 
 namespace nav_system
 {
-// A component: basically a node packaged as a loadable plugin
+// A component = basically a node packaged as a loadable plugin
 class NavActionServerComponent : public rclcpp::Node           
 {
 public:
   using NavigateToPose = nav_interfaces::action::NavigateToPose;
-  using GoalHandleNavigateToPose = rclcpp_action::ServerGoalHandle<NavigateToPose>;
+  using GoalHandleNavigateToPose = rclcpp_action::ServerGoalHandle<NavigateToPose>;   // type for a specific accepted goal.
 
-  explicit NavActionServerComponent(const rclcpp::NodeOptions & options);
+  explicit NavActionServerComponent(const rclcpp::NodeOptions & options);   // Constructor
 
-private:
-  rclcpp_action::Server<NavigateToPose>::SharedPtr action_server_;   // shared pointer to the action server 
-
+private:                     // Private variables
+  rclcpp_action::Server<NavigateToPose>::SharedPtr action_server_;  // shared pointer to the action server 
+  
+  // Called when a new goal request arrives
   rclcpp_action::GoalResponse handle_goal(
     const rclcpp_action::GoalUUID & uuid,
     std::shared_ptr<const NavigateToPose::Goal> goal);
-
+ 
+  // Called when user requests cancellation
   rclcpp_action::CancelResponse handle_cancel(
     const std::shared_ptr<GoalHandleNavigateToPose> goal_handle);
 
+  // Called when goal is accepted
   void handle_accepted(const std::shared_ptr<GoalHandleNavigateToPose> goal_handle);
 
+  // Contains actual navigation logic
   void execute(const std::shared_ptr<GoalHandleNavigateToPose> goal_handle);
 };
 
--- nav_system/src/nav_action_server_component.cpp
+++ nav_system/src/nav_action_server_component.cpp
@@ -1,4 +1,4 @@
-#include "nav_system/nav_action_server_component.hpp"
+#include "nav_system/nav_action_server_component.hpp"  // Including the header file 
 
 #include <chrono>
 #include <functional>
@@ -8,18 +8,19 @@
 
 using namespace std::chrono_literals;
 
-namespace nav_system
+namespace nav_system   // Namespace declaration
 {
-
+    
+    // Constructor - initializes the action server
     NavActionServerComponent::NavActionServerComponent(const rclcpp::NodeOptions & options)
-    : Node("nav_action_server_component", options)
+    : Node("nav_action_server_component", options)   // Because it takes NodeOptions, it can be loaded into a component container
     {
-    action_server_ = rclcpp_action::create_server<NavigateToPose>(
+    action_server_ = rclcpp_action::create_server<NavigateToPose>(     // create server that handles goal requests, cancellations, and feedback 
         this,
-        "navigate_to_pose",
-        std::bind(&NavActionServerComponent::handle_goal, this, std::placeholders::_1, std::placeholders::_2),
-        std::bind(&NavActionServerComponent::handle_cancel, this, std::placeholders::_1),
-        std::bind(&NavActionServerComponent::handle_accepted, this, std::placeholders::_1)
+        "navigate_to_pose",     // binding the action server to the "navigate_to_pose" custom action name
+        std::bind(&NavActionServerComponent::handle_goal, this, std::placeholders::_1, std::placeholders::_2),       // binding the handle_goal callback method to the action server
+        std::bind(&NavActionServerComponent::handle_cancel, this, std::placeholders::_1),  // binding the handle_cancel method to the action server
+        std::bind(&NavActionServerComponent::handle_accepted, this, std::placeholders::_1)   // binding the handle_accepted method to the action server
     );
 
     RCLCPP_INFO(this->get_logger(), "NavActionServerComponent started.");
@@ -50,22 +51,27 @@ namespace nav_system
     void NavActionServerComponent::handle_accepted(
     const std::shared_ptr<GoalHandleNavigateToPose> goal_handle)
     {
+        // Create a new thread to execute the goal in the background 
+                      // because execution is long-running, we would not want the acceptance callback itself to block.
         std::thread(
-            std::bind(&NavActionServerComponent::execute, this, std::placeholders::_1),
+            std::bind(&NavActionServerComponent::execute, this, std::placeholders::_1),  // bind the execute method to the action server
             goal_handle
-        ).detach();
+        ).detach();   // detach the thread to execute in the background
     }
 
     void NavActionServerComponent::execute(
     const std::shared_ptr<GoalHandleNavigateToPose> goal_handle)
     {
+        // Log the start of goal execution
         RCLCPP_INFO(this->get_logger(), "Executing goal...");
 
         auto feedback = std::make_shared<NavigateToPose::Feedback>();
         auto result = std::make_shared<NavigateToPose::Result>();
 
-        // Dummy execution loop to test the action lifecycle
+        // Dummy execution loop to test the action implementation
         for (int i = 0; i < 5; ++i) {
+
+            // check if cancel request has arrived
             if (goal_handle->is_canceling()) {
             result->success = false;
             result->final_x = 0.0;
@@ -73,11 +79,13 @@ namespace nav_system
             result->final_theta = 0.0;
             result->message = "Goal canceled during skeleton execution.";
 
+            // if so, mark the goal as canceled and return
             goal_handle->canceled(result);
-            RCLCPP_WARN(this->get_logger(), "Goal canceled.");
+            RCLCPP_WARN(this->get_logger(), "Goal canceled.");   
             return;
             }
 
+            // dummy feedback values to simulate progress toward the goal
             feedback->current_x = 0.1 * i;
             feedback->current_y = 0.0;
             feedback->current_theta = 0.0;
@@ -85,8 +93,10 @@ namespace nav_system
             feedback->heading_error = 0.0;
             feedback->phase = "SKELETON_EXECUTION";
 
+            // publish feedback to the client
             goal_handle->publish_feedback(feedback);
 
+            // log the feedback
             RCLCPP_INFO(
             this->get_logger(),
             "Publishing feedback: current_x=%.2f, distance_error=%.2f, phase=%s",
@@ -103,11 +113,11 @@ namespace nav_system
         result->final_theta = goal_handle->get_goal()->theta;
         result->message = "Skeleton execution completed successfully.";
 
-        goal_handle->succeed(result);
+        goal_handle->succeed(result);      // if no cancel request has arrived and goal is completed, the goal is marked as succeeded
 
         RCLCPP_INFO(this->get_logger(), "Goal succeeded.");
     }
 
 }  // namespace nav_system
 
-RCLCPP_COMPONENTS_REGISTER_NODE(nav_system::NavActionServerComponent)
+RCLCPP_COMPONENTS_REGISTER_NODE(nav_system::NavActionServerComponent)     // This line registers the class as a loadable ROS2 component
