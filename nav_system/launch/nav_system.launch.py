ddbc654a979afba84cd7f105091f54f8d2ed5c41
Mihret111 <mihret.kochito16@gmail.com>
2026-04-26 14:38:02 +0200

    create launch file and build the workflow

---
+++ nav_system/launch/nav_system.launch.py
@@ -0,0 +1,36 @@
+from launch import LaunchDescription
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+
+def generate_launch_description():
+    # launch server component alone first to see if it works
+    nav_server_component = ComposableNode(
+        package='nav_system',
+        plugin='nav_system::NavActionServerComponent',
+        name='nav_action_server_component'
+    )
+
+    # launch client component alone first to see if it works
+    nav_client_component = ComposableNode(
+        package='nav_system',
+        plugin='nav_system::NavActionClientComponent',
+        name='nav_action_client_component'
+    )
+
+    # launch both in a container
+    container = ComposableNodeContainer(
+        name='nav_system_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',       # multithreaded
+        composable_node_descriptions=[
+            nav_server_component,
+            nav_client_component,
+        ],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        container
+    ])
